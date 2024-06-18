301ed7bd70be2b92fc5537b12a031a07600ab351
Kalyani Gaikwad <kalyani.utkarsh.gaikwad@gmail.com>
2024-06-18 07:00:54 +0000

    app completed

---
+++ app.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv
+import os
+from langchain_groq import ChatGroq
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.messages import HumanMessage
+import gradio as gr 
+
+# Load api key to environment
+load_dotenv()
+os.environ['GROQ_API_KEY'] = os.getenv("GROQ_API_KEY")
+
+# Load groq model llama3
+model = ChatGroq(model_name='llama3-8b-8192')
+
+# Chat message history
+store = {}
+
+def get_session_history(session_id:str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+def get_model_response(message:str, history:str)->str:
+    with_message_history = RunnableWithMessageHistory(model, get_session_history)
+    config = {'configurable':{'session_id':'abc1'}}
+    response = with_message_history.invoke(
+        [HumanMessage(content=message)],
+        config = config
+    )
+    return response.content
+
+demo = gr.ChatInterface(fn=get_model_response, title='Groq Llama3 Chatbot')
+
+if __name__ == "__main__":
+    demo.queue().launch()
